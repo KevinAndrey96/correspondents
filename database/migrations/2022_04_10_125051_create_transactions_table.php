3d8323b5dcd17ee2c30bdb8f32fc443e0d7b930a
Cristian Delgado <filip2460@gmail.com>
2022-04-15 23:26:32 -0500

    feat: commision fields

--- database/migrations/2022_04_10_125051_create_transactions_table.php
+++ database/migrations/2022_04_10_125051_create_transactions_table.php
@@ -26,7 +26,11 @@ return new class extends Migration
             $table->string('status');
             $table->text('detail')->nullable();
             $table->string('boucher')->nullable();
-            $table->text('comment')->nullable():
+            $table->text('comment')->nullable();
+            $table->float('com_adm')->nullable();
+            $table->float('com_dis')->nullable();
+            $table->float('com_sup')->nullable();
+            $table->float('com_shp')->nullable();
             $table->timestamps();
             $table->foreign('shopkeeper_id')->references('id')->on('users');
         });
