ecad914cf33e7034858471356d012ddc72991b68
marjuliam <mjmartinezv28@gmail.com>
2022-06-11 11:51:03 -0500

    fix: users index view role name

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,7 +6,17 @@
                 <div class="card my-4">
                     <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                         <div class="bg-gradient-primary shadow-primary border-radius-lg pt-1 pb-0">
-                            <h6 class="text-white text-center text-capitalize ps-2 mx-6 ">Gestion de usuarios <a href="/user/create?role={{$role}}" class="btn btn-block btn-Secondary"><i style="color: white; margin-top: 13px;" class="material-icons opacity-10">group_add</i></a></h6>
+                            <h6 class="text-white text-center text-capitalize ps-2 mx-6 ">Gestion de usuarios 
+                                @if ($role == 'Shopkeeper')
+                                    (Tenderos)
+                                @elseif ($role == 'Distributor')
+                                    (Distribuidores)
+                                @elseif ($role == 'Supplier')
+                                    (Proveedores)
+                                @elseif ($role == 'Aministrator')
+                                    (Administradores)
+                                @endif
+                                <a href="/user/create?role={{$role}}" class="btn btn-block btn-Secondary"><i style="color: white; margin-top: 13px;" class="material-icons opacity-10">group_add</i></a></h6>
 
                         </div>
                     </div>
