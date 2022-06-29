d3372cd324f2f436f1295333241ee2a0254d2c96
Kevin Andrey Herrera Silva <kherrera@merqueo.com>
2022-06-28 21:45:25 -0500

    feat: added aditional fields to excel profits

--- resources/views/profit/excelExport.blade.php
+++ resources/views/profit/excelExport.blade.php
@@ -3,24 +3,41 @@
         <tr>
             @hasrole('Administrator')
             <th>Usuario</th>
+            <th>Rol</th>
             <th>Saldo Acumulado</th>
             @endhasrole
             <th>N° de Solicitud</th>
             <th>Monto a retirar</th>
-            <th>Fecha</th>
+            <th>Fecha y hora</th>
+            <th>Información</th>
             <th>¿Aceptada?</th>
         </tr>
     </thead>
     <tbody>
-        @foreach ($profits as $profit)       
+        @foreach ($profits as $profit)
         <tr>
             @hasrole('Administrator')
-            <td >{{ $profit->user->name}}</td>
-            <td >{{ $profit->user->profit}}</td>
+            <td>{{ $profit->user->name}}</td>
+            <td>
+                @if($profit->user->role == 'Administrator')
+                    Administrador
+                @endif
+                @if($profit->user->role == 'Distributor')
+                    Distribuidor
+                @endif
+                @if($profit->user->role == 'Supplier')
+                    Proveedor
+                @endif
+                @if($profit->user->role == 'Shopkeeper')
+                    Tendero
+                @endif
+            </td>
+            <td>{{ $profit->user->profit}}</td>
             @endhasrole
             <td>{{ $profit->id}}</td>
             <td>{{ $profit->amount}}</td>
-            <td>{{ $profit->date}}</td>
+            <td>{{ $profit->created_at}}</td>
+            <td>{{ $profit->extra}}</td>
             @if($profit->is_valid == 0)
             <td>No</td>
             @else
