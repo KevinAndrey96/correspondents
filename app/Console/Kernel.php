b5f842d9c17d2205064e2e533cfac1fde68e0696
Kevin Andrey Herrera Silva <kherrera@merqueo.com>
2022-09-07 18:32:43 -0500

    fix: now the scheduler runs each 5 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('transaction:reassign')
-            ->everyMinute()
+            ->everyFiveMinutes()
             ->timezone('America/Bogota')
             ->emailOutputTo('aspasoluciones@gmail.com', true);
     }
