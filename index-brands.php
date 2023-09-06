73f40598bd91aa6ba6c4d5dd5a984c22fc390c7a
Cristian Delgado <filip2460@gmail.com>
2023-09-05 23:57:15 -0500

    feat: add index-brand.php file

---
+++ index-brands.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Transacciones Corresponsales</title>
+    <style>
+        body, html, iframe {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            width: 100%;
+            overflow: hidden;
+            display: block;
+            position: relative;
+        }
+    </style>
+</head>
+<body>
+<iframe src="https://testing.asparecargas.net/?brand=aspa.instanceshape.com" frameborder="0"></iframe>
+</body>
+</html>
